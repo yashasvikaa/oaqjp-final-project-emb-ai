e440ae9e3d8013efadfa78b3eb45da734098e963
Yashasvika Ahuja <yashasvikaahuja@gmail.com>
2025-07-09 11:47:55 +0530

    Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,16 +1,40 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
-    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {
         "Content-Type": "application/json",
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
     }
-    input_json = {
-        "raw_document": {
-            "text": text_to_analyze
-        }
-    }
+    input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, headers=headers, json=input_json)
-    return response.text
+
+    result = json.loads(response.text)
+    emotions = result['emotionPredictions'][0]['emotion']
+
+    anger_score = emotions.get('anger', 0)
+    disgust_score = emotions.get('disgust', 0)
+    fear_score = emotions.get('fear', 0)
+    joy_score = emotions.get('joy', 0)
+    sadness_score = emotions.get('sadness', 0)
+
+    scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    dominant_emotion = max(scores, key=scores.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }  
